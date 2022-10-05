3bc676288d01777ea54b8d2db705e24f6f0c8fd5
Alex Truong <87406255+alextruonglt@users.noreply.github.com>
2022-10-05 15:47:07 -0700

    added CRUD to ticket functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import Login from './pages/Login'
 import Register from './pages/Register'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import NewTicket from './pages/NewTicket'
+import PrivateRoute from './components/PrivateRoute'
 
 
 
@@ -20,6 +22,9 @@ const App = () => {
                     <Route path='/' element={<Home/>} />
                     <Route path='/login' element={<Login/>} />
                     <Route path='/register' element={<Register/>} />
+                    <Route path='/new-ticket' element={<PrivateRoute />}>
+              <Route path='/new-ticket' element={<NewTicket />} />
+            </Route>
                 </Routes>
             </div>
         </Router>
