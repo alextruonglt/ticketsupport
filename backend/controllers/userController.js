5513bc1be46dd4304986d02ec5e7303e6b2dd39b
Alex Truong <87406255+alextruonglt@users.noreply.github.com>
2022-10-05 10:52:54 -0700

    added login functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler")
 const bcrypt = require("bcryptjs")
 const User = require("../models/userModel")
+const jwt = require("jsonwebtoken")
 
 // @ desc Regiser a new user
 // @route /api/users
@@ -39,7 +40,8 @@ const registerUser = asyncHandler(async(req, res ) =>{
         res.status(201).json({
             _id: user._id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateToken(user._id)
         })
     } else{
         res.status(400)
@@ -55,11 +57,46 @@ const registerUser = asyncHandler(async(req, res ) =>{
 // @route /api/users/login
 // @access Public 
 const loginUser = asyncHandler (async (req, res ) =>{
-    res.send("login Route")
+
+    const {email, password} = req.body
+    
+    const user = await User.findOne({email})
+
+    //check user and passwords match
+    if(user && (await bcrypt.compare(password, user.password))){
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id)
+        })
+    } else{
+        res.status(401)
+        throw new Error("Invalid credentials")
+    }
 })
+
+
+// @ desc Get Current user
+// @route /api/users/me
+// @access Private 
+const getMe = asyncHandler (async (req, res ) =>{
+    res.send("me")
+})
+
+
+
+//Generate token
+
+const generateToken = (id) =>{
+    return jwt.sign({id}, process.env.JWT_SECRET, {
+        expiresIn: "30d",
+    })
+}
 
 
 module.exports = {
     registerUser,
-    loginUser
+    loginUser,
+    getMe
 }
